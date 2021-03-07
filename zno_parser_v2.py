0260ce3e13b059403268ceaed1ae09eb3a217ade
cheml-beep <pipapetih@gmail.com>
2021-03-07 14:06:39 +0200

    minor refactoring

--- zno_parser_v2.py
+++ zno_parser_v2.py
@@ -12,7 +12,7 @@ def main():
     soup = BeautifulSoup(data, "html.parser")
     for pre_link in soup.find_all("div", class_="explanation"):
       link = pre_link.find_all("img")[0]["src"]
-      file_name = link.split('math-')[-1] if len(link.split('math-')) > 1 else link.split('/')[-1];
+      file_name = link.split('math-')[-1] if len(link.split('math-')) > 1 else link.split('/')[-1]
       urllib.request.urlretrieve("https://zno.osvita.ua" + link, __file__.split(os.path.basename(__file__))[0] + "/" + file_name)
       print(link)
  
